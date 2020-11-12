599e26056c3d80a12a3435a1e8297d106324b561
amedrod609 <antmedrod@gmail.com>
2020-11-12 18:55:01 +0100

    hasta 2 de relación 5

--- Relacion4/Ejer10.js
+++ Relacion4/Ejer10.js
@@ -7,23 +7,45 @@ class Pelicula{
         this.pais = pais;
         this.genero = genero;
         this.calificacion = calificacion;
+        
+        checkId(id);
+        checkTitulo(titulo);
+        checkAnio(director);
+
     }
 
-    checkId(){
-        if(this.id.match(/^[a-z]{2}\d{7}$/)){
+}
 
-        }
+
+function checkId(id){
+    if(this.id.match(/^[a-z]{2}\d{7}$/)){
+        document.write('ole ole ta bien')
     }
-
-    checkTitulo(){
-        if(this.titulo.match(/^\w{1,100}$/)){
-
-        }
-    }
-
-    checkAnio(){
-        if(this.anio.match(/^\d{4}$/)){
-            
-        }
+    else{
+        document.write('mu mal un 3')
     }
 }
+
+function checkTitulo(titulo){
+    if(this.titulo.match(/^\w{1,100}$/)){
+
+        document.write('ole ole ta bien')
+    }
+    else{
+        document.write('mu mal un 3')
+    }
+}
+
+function checkAnio(anio){
+    if(this.anio.match(/^\d{4}$/)){
+        
+        document.write('ole ole ta bien')
+    }
+    else{
+        document.write('mu mal un 3')
+    }
+}
+
+var pelicula1 = new Pelicula(
+    
+);
